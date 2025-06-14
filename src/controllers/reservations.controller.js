fb2b7a7b4f7213d722a4a41ce42ff5b1b3ecae12
Dani Araya <carayacn@gmail.com>
2025-06-14 12:24:36 -0600

    datos en experiencia

--- src/controllers/reservations.controller.js
+++ src/controllers/reservations.controller.js
@@ -144,7 +144,7 @@ exports.create = async (req, res) => {
 exports.listMine = async (req, res) => {
   const userId = req.user.id;
   const [rows] = await db.query(
-    `SELECT r.*, e.nombre AS experiencia
+    `SELECT r.*, e.nombre AS experiencia, e.ubicacion
        FROM reservations r
        JOIN experiences e ON e.id = r.experience_id
       WHERE r.user_id = ?
